54edf8c5b08ce73023d1d65175c79270a3e48bb8
Igor Tomov <itomov@griddynamics.com>
2018-05-18 14:08:55 +0300

    Updated `init-app` implementation (moved reading of config path from cli args to outside).

--- dist/lib/router/ui-router.factory.js
+++ dist/lib/router/ui-router.factory.js
@@ -13,10 +13,6 @@ var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
 
-var _glob = require('glob');
-
-var _glob2 = _interopRequireDefault(_glob);
-
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
@@ -29,34 +25,8 @@ var _defaultUiRouteHandler2 = _interopRequireDefault(_defaultUiRouteHandler);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function resolvePagesSettings(rootDir) {
-  var cwd = _path2.default.join(rootDir, 'src', 'ui');
-  return {
-    cwd: cwd,
-    routes: _glob2.default.sync('**/*.page.js', { cwd: cwd }).map(function (pagePath) {
-      var dirPath = _path2.default.dirname(pagePath, '.page.js');
-      var name = _path2.default.basename(pagePath, '.page.js');
-      var routePath = _path2.default.join(dirPath, name + '.route.js');
-      var clientPath = _path2.default.join(dirPath, name + '.client.js');
-      var dirPieces = dirPath.replace(/^\./, '').split('/');
-      var namespace = dirPieces;
-
-      if (namespace[namespace.length - 1] !== name) {
-        namespace.push(name);
-      }
-
-      return {
-        name: name,
-        namespace: namespace.join('.'),
-        routePath: routePath,
-        pagePath: pagePath,
-        clientPath: clientPath,
-        dirPath: dirPath,
-        dirPieces: dirPieces
-      };
-    })
-  };
-}
+var _require = require('ui-platform-core/dist/lib/pages/pages-settings.resolver'),
+    resolvePagesSettings = _require.resolvePagesSettings;
 
 var uiRouterFactory = exports.uiRouterFactory = function uiRouterFactory(_ref) {
   var rootDir = _ref.rootDir,
